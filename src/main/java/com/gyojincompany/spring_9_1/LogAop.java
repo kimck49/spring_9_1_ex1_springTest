265c071fb7eaf70816e6b1aad2e8938ab713c93e
kimck49 <kimck49@naver.com>
2022-05-16 10:17:47 +0900

    AOP 연습예제 01

---
+++ src/main/java/com/gyojincompany/spring_9_1/LogAop.java
@@ -0,0 +1,25 @@
+package com.gyojincompany.spring_9_1;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LogAop {
+	
+	public Object loggerAop(ProceedingJoinPoint joinpoint) throws Throwable {
+		
+		String signatureStr = joinpoint.getSignature().toShortString();
+		//공통기능이 적용될 메서드(joinpoint)의 이름 불러오기
+		System.out.println(signatureStr + " 메서드가 시작되었습니다!");
+		//joinpoint(메서드)의 이름 출력
+		long st = System.currentTimeMillis();//현재시간가져오기
+		
+		try {
+			Object obj = joinpoint.proceed();//joinpoint 메서드 실행
+			return obj;
+		} finally {
+			long et = System.currentTimeMillis();//현재시간가져오기
+			System.out.println(signatureStr + " 메서드가 종료되었습니다!");
+			System.out.println(signatureStr + "경과시간 : " + (et-st));
+		}
+	}
+	
+}
